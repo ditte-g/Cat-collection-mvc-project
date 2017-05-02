88f9f4d73f72a5effb656990df5506428912f08c
Ditte Gunterberg <ditte-g@users.noreply.github.com>
2017-05-02 11:20:56 +0200

    index

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+require ('resources/db.php');
+require ('resources/controller.php');
+require ('model/model.php');
+require ('model/cat.php');
+
+$config = require('resources/config.php');
+$db = new Database($config);
+$controller = new Controller($db);
+$controller->index();
