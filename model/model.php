e9d4c3d25bea95a33fc8f32708b8d1063f9dd3fe
Ditte Gunterberg <ditte-g@users.noreply.github.com>
2017-05-05 10:17:06 +0200

    added createCat function

--- model/model.php
+++ model/model.php
@@ -23,4 +23,12 @@ class Model
         }, $cat);
         return $cat;
     }
+
+    public function createCat($name, $birthday, $breed)
+    {
+        $create_stm = $this->db->prepare("INSERT INTO `cats` (`name`, `birthday`, `breed`) VALUES(:name, :birthday, :breed)");
+        $create_stm->execute(['name' => $name, 'birthday' => $birthday, 'breed' => $breed]);
+        return $create_stm;
+
+    }
 }
