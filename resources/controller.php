fc8091ae15a441af226b65df3927433c12f8bf3c
Ditte Gunterberg <ditte-g@users.noreply.github.com>
2017-05-10 22:50:02 +0200

    Update function and get by id

--- resources/controller.php
+++ resources/controller.php
@@ -15,22 +15,40 @@ class Controller
         if (empty($page))
             require('view/view.php');
         elseif ($page === "view") {
-            if (isset($_POST['delete'])) {
-                $id = $_POST['delete'];
-                $this->deleteCat($id);
-                header('Location: /index.php');
-                exit();
-            }
             require('view/view.php');
         } elseif ($page === 'create') {
-            require('model/create.php');
+            require('view/create.php');
         } elseif ($page === 'add') {
             $cat = new Cat($_POST['name'], $_POST['birthday'], $_POST['breed']);
             $success = $this->createCat($cat);
             header('Location: /index.php');
+        } elseif ($page === 'delete') {
+            $id = $_GET['id'];
+            $this->deleteCat($id);
+            header('Location: /index.php');
+            exit();
+        } elseif ($page === 'update') {
+            if (!empty($_POST)) {
+                $cat = $this->getById($_POST['id']);
+                $cat->setName($_POST['name']);
+                $cat->setBirthday($_POST['birthday']);
+                $cat->setBreed($_POST['breed']);
+                $this->updateCat($cat);
+                header('Location: /index.php');
+                exit();
+            } else {
+                $id = $_GET['id'];
+                $cat = $this->getById($id);
+                require('view/update.php');
+            }
         } else {
-
         }
+    }
+
+
+    public function getById($id)
+    {
+        return $this->model->getById($id);
     }
 
     public function getAllCats()
@@ -47,6 +65,11 @@ class Controller
     public function deleteCat($id)
     {
         return $this->model->deleteCat($id);
+    }
+
+    public function updateCat($cat)
+    {
+        return $this->model->updateCat($cat);
     }
 }
 
