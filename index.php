311992f437bfaaecb5232878425b772ee66d0131
Ditte Gunterberg <ditte-g@users.noreply.github.com>
2017-06-14 11:45:44 +0200

    update index

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ require('model/Database.php');
 require('controller/Controller.php');
 require ('model/model.php');
 require ('model/cat.php');
+require ('model/owner.php');
 
 $config = require('resources/config.php');
 $db = new Database($config);
