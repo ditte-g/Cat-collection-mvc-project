51eed96db5fac5d6dfd93463b40ae3c05b5da70b
Ditte Gunterberg <ditte-g@users.noreply.github.com>
2017-06-14 11:32:35 +0200

    update footer

--- view/footer.php
+++ view/footer.php
@@ -1,4 +1,5 @@
-<script src="/js/vendor/jquery-3.2.1.min.js"></script>
-<script src="/js/vendor/bootstrap.min.js"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<script src="/js/bootstrap.min.js"></script>
+<script src="/js/less.js" type="text/javascript"></script>
 </body>
 </html>
